e4edf71fb9a0f6da762d16a64575b981498a8bb8
Coby Lim <cobysantoslim@gmail.com>
2022-12-05 16:38:29 -0800

    remove trailing slash in urls

--- locations/urls.py
+++ locations/urls.py
@@ -3,5 +3,5 @@ from locations import views
 
 urlpatterns = [
     path('v1/locations/', views.LocationList.as_view()),
-    path('v1/locations/<int:pk>/', views.LocationDetail.as_view()),
+    path('v1/locations/<int:pk>', views.LocationDetail.as_view()),
 ]
