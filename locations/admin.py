cd57663002e853252fa40388a5faa75a525e8804
Coby Lim <cobysantoslim@gmail.com>
2022-12-05 17:13:00 -0800

    add filters and displays on admin page

--- locations/admin.py
+++ locations/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Location
 
-# Register your models here.
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at', 'parking_type', 'number_of_spaces', 'is_verified')
+    list_filter = ['name', 'created_at', 'updated_at', 'parking_type', 'number_of_spaces', 'is_verified']
 
-admin.site.register(Location)
+admin.site.register(Location, LocationAdmin)
